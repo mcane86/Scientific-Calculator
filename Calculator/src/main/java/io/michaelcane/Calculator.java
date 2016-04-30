fa38387b75eab631f88612decd00b9c4807b4751
Michael Cane <michaelcane@Zipcoder06s-MacBook-Pro.local>
2016-04-30 15:28:12 -0400

    fourth commit - more functions being added

--- Calculator/src/main/java/io/michaelcane/Calculator.java
+++ Calculator/src/main/java/io/michaelcane/Calculator.java
@@ -36,11 +36,73 @@ public class Calculator {
         return value1 *= -1;
     }
 
-    public double sin(double value1) {
+    public double sine(double value1) {
         return Math.sin(value1);
     }
 
-    public double cosin(double value1) {
+    public double cosine(double value1) {
         return Math.cos(value1);
     }
+
+    public double tangent(double value1) {
+        return Math.tan(value1);
+    }
+
+    public double inverseSine(double value1) {
+        return Math.asin(value1);
+    }
+
+    public double inverseCosine(double value1) {
+        return Math.acos(value1);
+    }
+
+    public double inverseTangent(double value1) {
+        return Math.atan(value1);
+    }
+
+    public double logarithm(double value1) {
+        return Math.log10(value1);
+    }
+
+    public double naturalLogarithm(double value1) {
+        return Math.log(value1);
+    }
+
+    public double factorial(double value1) {
+        if(value1 == 1) {
+            return 1;
+        }
+        return value1 * (factorial(value1-1));
+    }
+
+    public double inverseNaturalLog(double value1) {
+        return Math.pow(Math.E, (Math.log(value1)));
+    }
+
+    public double inverseLogarithm(double value1) {
+        return Math.pow(10, (Math.log(value1)));
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- Calculator/src/test/java/io/michaelcane/CalculatorSpec.java
+++ Calculator/src/test/java/io/michaelcane/CalculatorSpec.java
@@ -109,7 +109,7 @@ public class CalculatorSpec {
          * of the side opposite a given angle (in a right triangle) to the hypotenuse.
          */
         double expectedValue = 0.8939966636005579;
-        double actualVaue = calculator.sin(90);
+        double actualVaue = calculator.sine(90);
         assertEquals("This should evaluate to 0.8939966636005579", expectedValue, actualVaue, delta);
     }
 
@@ -120,7 +120,125 @@ public class CalculatorSpec {
          * of the side adjacent to an acute angle (in a right-angled triangle) to the hypotenuse.
          */
         double expectedValue = -0.4480736161291702;
-        double actualValue = calculator.cosin(90);
+        double actualValue = calculator.cosine(90);
         assertEquals("This should evaluate to 0.4480736161291702", expectedValue, actualValue, delta);
     }
+
+    @Test
+    public void test$tangent() {
+        /**
+         * This method will evaluate a tangent line
+         */
+        double expectedValue = -1.995200412208242;
+        double actualValue = calculator.tangent(90);
+        assertEquals("This should evaluate to -1.995200412208242", expectedValue, actualValue, delta);
+    }
+
+    @Test
+    public void test$inverseSine() {
+        /**
+         * This will provide the inverse of the sine of the value provided.
+         */
+        double expectedValue = 0.14046141470985582;
+        double actualValue = calculator.inverseSine(0.14);
+        assertEquals("This should evaluate to .14046141470985582", expectedValue, actualValue, delta);
+
+    }
+
+    @Test
+    public void test$inverseCosin() {
+        /**
+         * This will provide the inverse of the cosine of the value provided.
+         */
+        double expectedValue = 1.4303349120850408;
+        double actualValue = calculator.inverseCosine(0.14);
+        assertEquals("This should evaluate to 1.4303349120850408", expectedValue, actualValue, delta);
+    }
+
+    @Test
+    public void test$inverseTangent() {
+        /**
+         * This will provide the inverse of the tangent of the value provided.
+         */
+        double expectedValue = 0.13909594148207133;
+        double actualValue = calculator.inverseTangent(0.14);
+        assertEquals("This should evaluate to 0.13909594148207133", expectedValue, actualValue, delta);
+    }
+
+    @Test
+    public void test$logarithm() {
+        /**
+         * This will evaluate to the base 10 value of the number provided.
+         */
+        double expectedValue = 0.9542425094393249;
+        double actualValue = calculator.logarithm(9);
+        assertEquals("This should evaluate to 0.9542425094393249", expectedValue, actualValue, delta);
+    }
+
+    @Test
+    public void test$naturalLogarithm() {
+        /**
+         * This will evaluate to the invers of the Log.
+         */
+        double expectedValue = 2.1972245773362196;
+        double actualValue = calculator.naturalLogarithm(9);
+        assertEquals("This should evaluate to 2.1972245773362196", expectedValue, actualValue, delta);
+    }
+
+    @Test
+    public void test$factorial() {
+        /**
+         * This will give the factorial for the provided value.
+         */
+        double expectedValue = 24;
+        double actualValue = calculator.factorial(4);
+        assertEquals("This should evaluate to 24", expectedValue, actualValue, delta);
+    }
+
+    @Test
+    public void test$inverseNaturalLog() {
+        /**
+         * This will give the inverse for natual logarithms
+         */
+        double expectedValue = 1.9999999999999998;
+        double actualValue = calculator.inverseNaturalLog(2);
+        assertEquals("This should evaluate to ", expectedValue, actualValue, delta);
+    }
+
+    @Test
+    public void test$inverseLogarithm() {
+        /**
+         * This will give the inverse for logarithms
+         */
+        double expectedValue = 157.4796994747437;
+        double actualValue = calculator.inverseLogarithm(9);
+        assertEquals("This should evaluate to 1000000000", expectedValue, actualValue, delta);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
